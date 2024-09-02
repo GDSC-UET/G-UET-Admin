457f0edbf483ce777e7a03488caa21d5f737266d
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-02 22:00:54 +0700

    feat: all members page

---
+++ src/mock/members.ts
@@ -0,0 +1,37 @@
+export const mockMembers = [
+  {
+    id: 1,
+    position: 'Frontend Developer',
+    team: 'Technical',
+    gen: 'Gen 0',
+    school: 'University of Technology and Engineering'
+  },
+  {
+    id: 2,
+    position: 'Frontend Developer',
+    team: 'Technical',
+    gen: 'Gen 0',
+    school: 'University of Technology and Engineering'
+  },
+  {
+    id: 3,
+    position: 'Frontend Developer',
+    team: 'Technical',
+    gen: 'Gen 0',
+    school: 'University of Technology and Engineering'
+  },
+  {
+    id: 4,
+    position: 'Frontend Developer',
+    team: 'Technical',
+    gen: 'Gen 0',
+    school: 'University of Technology and Engineering'
+  },
+  {
+    id: 5,
+    position: 'Frontend Developer',
+    team: 'Technical',
+    gen: 'Gen 0',
+    school: 'University of Technology and Engineering'
+  }
+]
---
+++ src/pages/members/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <UContainer>
+    <PagesTable
+      :title="tablePagesData.title"
+      :create-button="tablePagesData.createButton"
+      :data-fields="tablePagesData.dataFields"
+      :data="tablePagesData.data"
+    />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import type { TablePages } from '~/types/table-pages'
+import { mockMembers } from '~/mock/members'
+
+const tablePagesData = ref<TablePages>({
+  title: 'All Members',
+  createButton: {
+    label: 'Add new member',
+    to: '/members/new'
+  },
+  dataFields: [
+    {
+      key: 'id',
+      label: 'Id',
+      sortable: false
+    },
+    {
+      key: 'name',
+      label: 'Name',
+      sortable: true
+    },
+    {
+      key: 'position',
+      label: 'Position',
+      sortable: true
+    },
+    {
+      key: 'gen',
+      label: 'Gen',
+      sortable: true
+    },
+    {
+      key: 'school',
+      label: 'School',
+      sortable: false
+    }
+  ],
+  data: mockMembers
+})
+</script>
