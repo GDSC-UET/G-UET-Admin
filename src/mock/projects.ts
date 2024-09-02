c1e3563fccbc11d505bfebcade15019402ed1d20
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-02 23:11:54 +0700

    feat: all projects page

---
+++ src/mock/projects.ts
@@ -0,0 +1,47 @@
+export const mockProjects = [
+  {
+    id: 1,
+    name: 'G-UET',
+    status: 'Developing',
+    startedAt: '21/08/2024',
+    finishedAt: '21/08/2024',
+    version: '1.0',
+    description: 'Lorem ipsum ipsum ipsum'
+  },
+  {
+    id: 2,
+    name: 'G-UET',
+    status: 'Developing',
+    startedAt: '21/08/2024',
+    finishedAt: '21/08/2024',
+    version: '1.0',
+    description: 'Lorem ipsum ipsum ipsum'
+  },
+  {
+    id: 3,
+    name: 'G-UET',
+    status: 'Developing',
+    startedAt: '21/08/2024',
+    finishedAt: '21/08/2024',
+    version: '1.0',
+    description: 'Lorem ipsum ipsum ipsum'
+  },
+  {
+    id: 4,
+    name: 'G-UET',
+    status: 'Developing',
+    startedAt: '21/08/2024',
+    finishedAt: '21/08/2024',
+    version: '1.0',
+    description: 'Lorem ipsum ipsum ipsum'
+  },
+  {
+    id: 5,
+    name: 'G-UET',
+    status: 'Developing',
+    startedAt: '21/08/2024',
+    finishedAt: '21/08/2024',
+    version: '1.0',
+    description: 'Lorem ipsum ipsum ipsum'
+  }
+]
