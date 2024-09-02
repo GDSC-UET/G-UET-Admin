248a9f829190cf9cb463f6488b5f30f7aef4994c
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-02 22:46:02 +0700

    feat: all teams page

---
+++ src/pages/teams/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <UContainer>
+    <PagesTable
+      :title="tablePagesData.title"
+      :create-button="tablePagesData.createButton"
+      :data-fields="tablePagesData.dataFields"
+      :data="tablePagesData.data"
+    />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import { mockTeams } from '~/mock/teams'
+import type { TablePages } from '~/types/table-pages'
+
+const tablePagesData = ref<TablePages>({
+  title: 'All Positions',
+  createButton: {
+    label: 'Add new team',
+    to: '/teams/new'
+  },
+  dataFields: [
+    {
+      key: 'id',
+      label: 'Id',
+      sortable: false
+    },
+    {
+      key: 'name',
+      label: 'Team',
+      sortable: true
+    },
+    {
+      key: 'positions',
+      label: 'Positions',
+      sortable: true
+    }
+  ],
+  data: mockTeams
+})
+</script>
