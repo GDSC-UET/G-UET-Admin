a598a7b7d24409042f0f5ab8c81c0407756b3b24
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-02 17:40:01 +0700

    feat: form pages component

---
+++ src/components/pages/form.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mt-16 flex flex-col items-center gap-8">
+    <span class="text-4xl font-medium">{{ title }}</span>
+    <div class="flex w-[800px] flex-col items-center gap-8">
+      <slot />
+      <div class="flex flex-col gap-2">
+        <UButton
+          :ui="{ rounded: 'rounded-full' }"
+          size="lg"
+          :label="submitButtonLabel"
+          block
+          class="w-[300px]"
+          :color="action === 'create' ? 'primary' : 'yellow'"
+          @click="handleSubmit"
+        />
+        <UButton
+          :ui="{ rounded: 'rounded-full' }"
+          size="lg"
+          label="Cancel"
+          class="w-[300px]"
+          block
+          color="gray"
+          @click="handleCancel"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { FormPages } from '~/types/form-pages'
+
+defineProps<FormPages>()
+
+const emit = defineEmits<{
+  (e: 'submit'): void
+  (e: 'cancel'): void
+}>()
+
+const handleSubmit = () => {
+  emit('submit')
+}
+
+const handleCancel = () => {
+  emit('cancel')
+}
+</script>
---
+++ src/pages/jobs/new/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <PagesForm
+    title="Add new job"
+    submit-button-label="Update job"
+    action="update"
+    @submit="handleSubmit"
+    @cancel="handleCancel"
+  >
+    Haha
+  </PagesForm>
+</template>
+
+<script setup lang="ts">
+const handleSubmit = () => {
+  console.log('submit')
+}
+
+const handleCancel = () => {
+  console.log('cancel')
+}
+</script>
