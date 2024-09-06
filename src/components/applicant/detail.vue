cc8c016bb7d672e7e17d6ac4f7c7be9e20aaeee4
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-06 09:44:19 +0700

    feat: applicant detail

---
+++ src/components/applicant/detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="my-16 flex flex-col gap-8">
+    <div class="flex items-center justify-between">
+      <span class="text-4xl font-medium">{{ name }}</span>
+      <div class="flex items-center gap-4">
+        <span>Status: {{ status }}</span>
+        <UButton color="green" label="Shortlist" />
+        <UButton color="red" label="Decline" />
+      </div>
+    </div>
+    <span class="text-xl font-medium">{{ email }} • {{ phoneNumber }}</span>
+    <div class="flex items-center gap-4">
+      <span class="text-xl font-medium">{{ position.name }}</span>
+      <TeamBadge :label="position.team" />
+    </div>
+    <span class="text-xl font-medium">Applied at {{ format(appliedAt, 'mm/dd/yyyy') }}</span>
+    <span class="text-xl font-medium">{{ school }}</span>
+    <p><span class="text-xl font-medium">Link CV: </span>{{ linkCV }}</p>
+    <p><span class="text-xl font-medium">Notes: </span>{{ notes }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ApplicantDetail } from '~/types/applicants'
+import { format } from 'date-fns'
+
+defineProps<ApplicantDetail>()
+</script>
--- src/mock/applicants.ts
+++ src/mock/applicants.ts
@@ -35,3 +35,19 @@ export const mockApplicants = [
     appliedAt: '21/08/2024'
   }
 ]
+
+export const mockApplicant = {
+  id: 1,
+  name: 'Tran Manh Dung',
+  status: 'Applied',
+  email: 'g-uet@gmail.com',
+  phoneNumber: '0123456789',
+  school: 'UET',
+  position: {
+    name: 'Frontend Developer',
+    team: 'Technical'
+  },
+  appliedAt: new Date('08/24/2024'),
+  linkCV: 'https://github.com',
+  notes: 'Lorem ipsum ipsum'
+}
