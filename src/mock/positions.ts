95e7f7e6835c7bf8aab9f1f1e2b9af5679a86529
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-06 10:09:32 +0700

    feat: position detail

--- src/mock/positions.ts
+++ src/mock/positions.ts
@@ -35,3 +35,47 @@ export const mockPositions = [
     members: 10
   }
 ]
+
+export const mockPosition = {
+  id: 1,
+  name: 'Frontend Developer',
+  team: 'Technical',
+  responsibilities: 'Lorem ipsum ipsum ipsum\nLorem ipsum ipsum ipsum',
+  members: [
+    {
+      id: 1,
+      name: 'Tran Manh Dung',
+      position: 'Leader',
+      gen: 0,
+      school: 'UET'
+    },
+    {
+      id: 2,
+      name: 'Tran Manh Dung',
+      position: 'Leader',
+      gen: 0,
+      school: 'UET'
+    },
+    {
+      id: 3,
+      name: 'Tran Manh Dung',
+      position: 'Leader',
+      gen: 0,
+      school: 'UET'
+    },
+    {
+      id: 4,
+      name: 'Tran Manh Dung',
+      position: 'Leader',
+      gen: 0,
+      school: 'UET'
+    },
+    {
+      id: 5,
+      name: 'Tran Manh Dung',
+      position: 'Leader',
+      gen: 0,
+      school: 'UET'
+    }
+  ]
+}
